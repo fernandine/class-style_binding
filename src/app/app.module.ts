5c10b627b887810453e183439119a0de577c2fcf
Jean Fernandine <jfernandine@gmail.com>
2022-04-16 15:17:51 -0300

    class_e_style-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MeuSiteComponent } from './meu-site/meu-site.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeuSiteComponent
   ],
   imports: [
     BrowserModule
